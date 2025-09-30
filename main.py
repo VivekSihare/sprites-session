1d5cfbbfb7e9cfa974ebf29caf9f7252b7a7bff1
VivekSihare <viveksihare.github@gmail.com>
2025-09-30 18:01:36 +0100

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-scene.set_background_color(2)
+# What happens if player overlaps food
+
+def on_on_overlap(player2, food):
+    food.set_position(randint(10, 150), randint(10, 110))
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_on_overlap)
+
+# Set background
+scene.set_background_color(9)
+# Create player
 newPlayer = sprites.create(img("""
         . . . . . . . .
         . . 2 2 2 2 . .
@@ -11,3 +19,34 @@ newPlayer = sprites.create(img("""
         """),
     SpriteKind.player)
 newPlayer.set_position(80, 60)
+# Add an enemy sprite
+enemy = sprites.create(img("""
+        . . . . . . . .
+        . . e e e e . .
+        . e e e e e e .
+        . e 9 e e 9 e .
+        . e e e e e e .
+        . . e e e e . .
+        . . e . . e . .
+        . . . . . . . .
+        """),
+    SpriteKind.enemy)
+enemy.set_position(30, 30)
+enemy.vx = 50
+# enemy moves left-right
+enemy.vy = 50
+# Bounce enemy off walls
+enemy.set_bounce_on_wall(True)
+# Add a collectible coin
+coin = sprites.create(img("""
+        . . . . . . . .
+        . . 5 5 5 5 . .
+        . 5 5 5 5 5 5 .
+        . 5 5 5 5 5 5 .
+        . 5 5 5 5 5 5 .
+        . . 5 5 5 5 . .
+        . . . 5 5 . . .
+        . . . . . . . .
+        """),
+    SpriteKind.food)
+coin.set_position(120, 80)
--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-scene.setBackgroundColor(1)
+// What happens if player overlaps food
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (player2, food) {
+    food.setPosition(randint(10, 150), randint(10, 110))
+})
+// Set background
+scene.setBackgroundColor(9)
+// Create player
 let newPlayer = sprites.create(img`
     . . . . . . . . 
     . . 2 2 2 2 . . 
@@ -10,3 +16,32 @@ let newPlayer = sprites.create(img`
     . . . . . . . . 
     `, SpriteKind.Player)
 newPlayer.setPosition(80, 60)
+// Add an enemy sprite
+let enemy = sprites.create(img`
+    . . . . . . . . 
+    . . e e e e . . 
+    . e e e e e e . 
+    . e 9 e e 9 e . 
+    . e e e e e e . 
+    . . e e e e . . 
+    . . e . . e . . 
+    . . . . . . . . 
+    `, SpriteKind.Enemy)
+enemy.setPosition(30, 30)
+enemy.vx = 50
+// enemy moves left-right
+enemy.vy = 50
+// Bounce enemy off walls
+enemy.setBounceOnWall(true)
+// Add a collectible coin
+let coin = sprites.create(img`
+    . . . . . . . . 
+    . . 5 5 5 5 . . 
+    . 5 5 5 5 5 5 . 
+    . 5 5 5 5 5 5 . 
+    . 5 5 5 5 5 5 . 
+    . . 5 5 5 5 . . 
+    . . . 5 5 . . . 
+    . . . . . . . . 
+    `, SpriteKind.Food)
+coin.setPosition(120, 80)
